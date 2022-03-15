d4a66841865331aa083fb2013bed3a4debd47624
jailsondamasceno <jailsonengenharia1@gmail.com>
2022-03-15 17:16:22 -0300

    added new props tasks

--- toto-blockchain/contracts/TodoList.sol
+++ toto-blockchain/contracts/TodoList.sol
@@ -5,38 +5,50 @@ contract TodoList {
 
   struct Task {
     uint id;
-    string content;
-    bool completed;
+    string title;
+    string description;
+    string priority;
+    string status;
+    uint date;
   }
 
   mapping(uint => Task) public tasks;
 
   event TaskCreated(
     uint id,
-    string content,
-    bool completed
+    string title,
+    string description,
+    string priority,
+    string status,
+    uint  date
   );
 
   event TaskCompleted(
     uint id,
-    bool completed
+    string status
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+ createTask(
+      "My first task",
+      "My first task description",
+      "hight",
+      "0",
+      block.timestamp
+    );
   }
 
-  function createTask(string memory _content) public {
+  function createTask( string memory _title,  string memory _description,  string memory _priority, string memory _status, uint  _date) public {
     taskCount ++;
-    tasks[taskCount] = Task(taskCount, _content, false);
-    emit TaskCreated(taskCount, _content, false);
+    tasks[taskCount] = Task(taskCount, _title, _description, _priority, _status, _date);
+    emit TaskCreated(taskCount, _title,_description,_priority,_status, _date);
   }
 
   function toggleCompleted(uint _id) public {
     Task memory _task = tasks[_id];
-    _task.completed = !_task.completed;
+    _task.status = keccak256(bytes(_task.status))  == keccak256(bytes("1")) ? '0' : '1';
     tasks[_id] = _task;
-    emit TaskCompleted(_id, _task.completed);
+    emit TaskCompleted(_id, _task.status);
   }
 
 }
